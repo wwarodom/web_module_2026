c5274241b5bde65d3293a5bfbb581b0f7de93dae
Warodom Werapun <wwarodom@gmail.com>
2026-01-27 14:23:42 +0700

    add tailwinds

--- 6_next_basic/my-app/app/props/page.tsx
+++ 6_next_basic/my-app/app/props/page.tsx
@@ -1,25 +1,53 @@
 const STYLE = "text-2xl text-blue-800 border-4 rounded-xl m-2 mt-4 p-2"
 export default function PropPage() {
+
+    const myPrint = () => console.log("Hey!!!")
+    // myPrint()
+
     return <div><h1>Props Page</h1>
-        <A foo="foo message" />
-        <br />
-        <B bar="bar message" />
+        <div className="flex justify-around items-center min-h-screen border-2">
+            <A
+                foo="foo message"
+                foo2="foo 2!!"
+                amount="300"
+            />
+            <br />
+            <B bar="bar message"
+                bar2="bar 2!!"
+                myPrint={myPrint}
+            />
+        </div>
     </div>
 }
 
-function A(props: { foo: string }) {
+function A(props: { foo: string, foo2: string, amount: string | number }) {
     return <div>A Component
         <h1 className={STYLE}>
-            {props.foo}
+            <div>
+                {props.foo}
+            </div>
+            <hr />
+            <div>
+                {props.foo2}
+            </div>
+            <div>
+                {+props.amount}
+            </div>
         </h1>
     </div>
 }
 
-function B({ bar }: { bar: string }) {
+function B({ bar, bar2, myPrint }: {
+    bar: string,
+    bar2: string,
+    myPrint: () => void
+}) {
+    myPrint()
     return <div>
         B Component
         <h1 className={STYLE}>
-            {bar}
+            <div>{bar}</div>
+            <div>{bar2}</div>
         </h1>
     </div>
 }
