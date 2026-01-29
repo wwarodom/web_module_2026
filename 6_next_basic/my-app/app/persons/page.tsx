da6a9cd0347a991fcb51fbc22c211553cf303653
Warodom Werapun <wwarodom@gmail.com>
2026-01-29 09:54:37 +0700

    json stringnify

--- 6_next_basic/my-app/app/persons/page.tsx
+++ 6_next_basic/my-app/app/persons/page.tsx
@@ -1,17 +1,18 @@
 'use client'
 import { useState } from "react"
-
 export default function PersonsPage() {
+    const [persons, setPersons] = useState([
+        { id: 1, name: "Warodom Werapun", age: 47 },
+        { id: 2, name: "John Travota", age: 32 },
+        { id: 3, name: "Joe Nuvo", age: 19 },
+    ])
 
-    const [persons, setPersons] = useState([1, 2, 3])
-
-    return <main className="bg-amber-50 min-h-screen">
-        <div>
-            <h1>Person</h1>
+    return <main className="flex justify-center items-center min-h-[95vh]">
+        <div className="bg-amber-50 p-32">
+            <h1 className="text-2xl mb-2">Person</h1>
             <div>
-                {persons}
+                {JSON.stringify(persons)}
             </div>
         </div>
-
     </main>
 }
