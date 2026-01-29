dda0d627d43418a57c1941667840a44b6b7ce31e
Warodom Werapun <wwarodom@gmail.com>
2026-01-29 16:20:33 +0700

    fetch example

---
+++ 6_next_basic/my-app/app/fetch/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react';
+
+type UserType = {
+    login: string
+    id: string
+    bio: string
+    avatar_url: string
+}
+
+export default function MyFetch() {
+
+    const [user, setUser] = useState('wwarodom')
+    const [data, setData] = useState<UserType>({})
+
+    const fetchUser = async () => {
+        const URL = `https://api.github.com/users/${ user }`
+        const response = await fetch(URL);
+        const data = await response.json()
+        console.log("Data: ", data)
+        setData(data)
+    }
+
+    return <div className='flex flex-col justify-between p-4 border-2 m-8 rounded text-lg shadow-2xl text-gray-600'>
+        <h1>Fetch</h1>
+        <div className='border bg-amber-100 p-4 rounded'>
+            <div>
+                <label htmlFor="user">User Name:</label>
+            </div>
+            <div>
+                <input
+                    className='border p-2 text-gray-600'
+                    type="text" name="user"
+                    onChange={(e) => setUser(e.target.value)}
+                />
+            </div>
+            <div>
+                <button
+                    className='border px-2 m-2 rounded'
+                    onClick={fetchUser}>Fetch</button>
+            </div>
+        </div>
+
+        <div className="flex justify-between p-4 border-2 m-8 rounded text-lg shadow-2xl text-gray-600">
+            <div>
+                <div>Login: {data.login}</div>
+                <div>Id: {data.id}</div>
+                <div>bio: {data.bio}</div>
+            </div>
+            <div>
+                <div>
+                    <Image src={data.avatar_url}
+                        width={100} height={100} alt="my photo" />
+                </div>
+            </div>
+        </div>
+    </div>
+}
--- 6_next_basic/my-app/next.config.ts
+++ 6_next_basic/my-app/next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       new URL('https://upload.wikimedia.org/**/**'),
-      new URL('https://computing.psu.ac.th/**/**')],
+      new URL('https://computing.psu.ac.th/**/**'),
+      // new URL('https://avatars.githubusercontent.com/**/**?v=4'),
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**/**',
+      }],
     dangerouslyAllowLocalIP: true
   },
 
